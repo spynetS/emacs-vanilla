f72af3a105c6b2f67d78da8e2036a9681f3f13cb
Alfred Roos <alfred@stensatter.se>
2025-01-09 20:01:38 +0100

    added c3

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers-type 'visual)
+ '(package-selected-packages
+   '(org-modern projectile lsp-ui flycheck which-key capo smex nyan-mode magit lsp-mode ido-completing-read+ gruber-darker-theme dash-functional corfu cape all-the-icons-nerd-fonts all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
